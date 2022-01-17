86f785acd4d28382bd7d28f90f18abd125d7189f
vikasshah11 <you@example.com>
2022-01-17 14:57:01 +0000

    added vpc module 1

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,15 @@
 module "vpc" {
-    source  = "terraform-google-modules/network/google"
-    version = "~> 3.0"
+  source  = "app.terraform.io/SummitEngineering/network/google"
+  version = "3.4.0"
+  # insert required variables here
 
-    project_id   = ""
-    network_name = "summit-network"
-    routing_mode = "GLOBAL"
+   project_id   = "var.project"
+   network_name = "summit-network"
+
+credentials "app.terraform.io" {
+    # valid user API token:
+    token = "xxxxxx.atlasv1.zzzzzzzzzzzzz"
+  }
 
 subnets = [
   {
@@ -14,32 +19,5 @@ subnets = [
   }
 ]
 
-secondary_ranges = {
-        subnet-01 = [
-            {
-                range_name    = "subnet-01-secondary-01"
-                ip_cidr_range = "192.168.64.0/24"
-            },
-        ]
-
-        subnet-02 = []
-    }
-
-    routes = [
-        {
-            name                   = "egress-internet"
-            description            = "route through IGW to access internet"
-            destination_range      = "0.0.0.0/0"
-            tags                   = "egress-inet"
-            next_hop_internet      = "true"
-        },
-{
-            name                   = "app-proxy"
-            description            = "route through proxy to reach app"
-            destination_range      = "10.50.10.0/24"
-            tags                   = "app-proxy"
-            next_hop_instance      = "app-proxy-instance"
-            next_hop_instance_zone = "us-west1-a"
-        },
-    ]
 }
+
