e35c8cfeb2469da6a88558d03c1ecf0786d804c3
vikasshah11 <you@example.com>
2022-01-17 14:12:02 +0000

    rollback

--- main.tf
+++ main.tf
@@ -47,7 +47,6 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
-  department = devops
 
   boot_disk {
     initialize_params {
