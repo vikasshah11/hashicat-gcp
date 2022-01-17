b9bc0b5678d80e9683d893b123ff63ca8b4f1eca
vikasshah11 <you@example.com>
2022-01-17 14:15:07 +0000

    adding required labels

--- main.tf
+++ main.tf
@@ -47,6 +47,8 @@ resource "google_compute_instance" "hashicat" {
   name         = "${var.prefix}-hashicat"
   zone         = "${var.region}-b"
   machine_type = var.machine_type
+  department   = devops
+  dillable     = true
 
   boot_disk {
     initialize_params {
